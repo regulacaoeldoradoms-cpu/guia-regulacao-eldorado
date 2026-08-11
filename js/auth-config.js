332d6a775e9b642263284ac2de09c5ceb2bfd8d2
regulacaoeldoradoms-cpu <regulacaoeldoradoms@gmail.com>
2026-08-11 15:10:47 -0400

    Ativar autenticação obrigatória no portal

--- js/auth-config.js
+++ js/auth-config.js
@@ -2,7 +2,7 @@
 
 window.REGULATION_AUTH_CONFIG = Object.freeze({
   endpoint: 'https://yellow-wave-d0a1guia-regulacao-ia.regulacaoeldoradoms.workers.dev',
-  enforcement: false,
+  enforcement: true,
   tokenStorageKey: 'regulacao.portal.session',
   userStorageKey: 'regulacao.portal.user',
   loginPath: '/login/',
