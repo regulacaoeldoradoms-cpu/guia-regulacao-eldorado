7364e985fefe7ecda1911b7189a63967879012b3
regulacaoeldoradoms-cpu <regulacaoeldoradoms@gmail.com>
2026-09-09 15:39:09 -0400

    Renovar cache para correção da fila de Altas V32

--- portal-sw.js
+++ portal-sw.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const CACHE_VERSION = '20260909-3';
+const CACHE_VERSION = '20260909-4';
 const STATIC_CACHE = `portal-static-${CACHE_VERSION}`;
 const PAGE_CACHE = `portal-pages-${CACHE_VERSION}`;
 const PORTAL_CACHE_PREFIXES = ['portal-static-', 'portal-pages-'];
