fde75c3917734e304fa3aef2876fb794ffb3308e
regulacaoeldoradoms-cpu <regulacaoeldoradoms@gmail.com>
2026-09-11 00:38:24 -0400

    Renovar cache da busca social e chat

--- portal-sw.js
+++ portal-sw.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const CACHE_VERSION = '20260911-2';
+const CACHE_VERSION = '20260911-3';
 const STATIC_CACHE = `portal-static-${CACHE_VERSION}`;
 const PAGE_CACHE = `portal-pages-${CACHE_VERSION}`;
 const PORTAL_CACHE_PREFIXES = ['portal-static-', 'portal-pages-'];
@@ -22,13 +22,15 @@ const CORE_RESOURCES = Object.freeze([
   '/ferramentas/',
   '/css/portal.css?v=20260816-5',
   '/css/home-loading.css?v=20260909-1',
-  '/css/social.css?v=20260910-3',
+  '/css/social.css?v=20260911-1',
   '/css/portal-pwa.css?v=20260910-2',
   '/js/auth-config.js?v=20260815-1',
   '/js/portal-performance.js?v=20260911-1',
   '/js/portal-pwa.js?v=20260911-1',
   '/js/auth-client.js?v=20260910-4',
   '/js/tools-catalog.js?v=20260910-2',
+  '/js/social-navigation.js?v=20260911-1',
+  '/js/portal-chat.js?v=20260911-1',
   '/assets/portal-regulacao-icon.webp?v=20260909-1',
   '/assets/portal-regulacao-header.png?v=20260910-1',
   '/portal.webmanifest?v=20260911-1',
