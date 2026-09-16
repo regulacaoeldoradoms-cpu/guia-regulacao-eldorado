dc978bc54d1ac49951471505b6f4a41e14c6b00b
regulacaoeldoradoms-cpu <regulacaoeldoradoms@gmail.com>
2026-09-16 13:49:25 -0400

    chore(central): renovar cache da correção de impressão

--- portal-sw.js
+++ portal-sw.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const CACHE_VERSION = '20260916-4';
+const CACHE_VERSION = '20260916-5';
 const STATIC_CACHE = `portal-static-${CACHE_VERSION}`;
 const PAGE_CACHE = `portal-pages-${CACHE_VERSION}`;
 const PORTAL_CACHE_PREFIXES = ['portal-static-', 'portal-pages-'];
