0c77f003816efc27bc701ecfc7c2dca1f57cf1cd
regulacaoeldoradoms-cpu <regulacaoeldoradoms@gmail.com>
2026-09-16 17:05:42 -0400

    test(central): servir SVGs do kit com MIME correto

--- testing/browser/serve-staging.mjs
+++ testing/browser/serve-staging.mjs
@@ -11,6 +11,7 @@ const types = new Map([
   ['.js', 'text/javascript; charset=utf-8'],
   ['.mjs', 'text/javascript; charset=utf-8'],
   ['.css', 'text/css; charset=utf-8'],
+  ['.svg', 'image/svg+xml'],
   ['.json', 'application/json; charset=utf-8'],
   ['.txt', 'text/plain; charset=utf-8'],
   ['.wasm', 'application/wasm'],
